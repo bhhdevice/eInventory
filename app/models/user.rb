991a62ad7cf87c64fb6dcc75e092e710df650394
goldenhub77 <sg300c@gmail.com>
2018-06-06 10:44:40 -0400

    made reports_to optional

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   belongs_to :department
   belongs_to :location
   belongs_to :job_title
-  belongs_to :reports_to, class_name: 'User', foreign_key: 'reports_to_id'
+  belongs_to :reports_to, class_name: 'User', foreign_key: 'reports_to_id', optional: true
 
   validates :first_name, presence: true
   validates :last_name, presence: true
@@ -19,6 +19,7 @@ class User < ApplicationRecord
   validates :city, presence: true, allow_blank: true
   validates :zip_code, presence: true, allow_blank: true, length: { is: 5 }
   validates :phone_number, presence: true, allow_blank: true
+  validates :reports_to, presence: true, allow_blank: true
   validates_inclusion_of :active, in: [true, false]
   validates_inclusion_of :admin, in: [true, false]
   validates_inclusion_of :manager, in: [true, false]
