f5c6ec1f4fb9405e5292aa4f791c5de2add6ed65
goldenhub77 <sg300c@gmail.com>
2018-06-09 15:35:15 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :employees, only: [:index, :show]
-  resources :job_titles, except: [:show]
-  resources :departments, except: [:show]
-  resources :locations, except: [:show]
   scope :admin do
     resources :users, controller: :users
+    resources :job_titles, except: [:show], controller: :job_titles
+    resources :departments, except: [:show], controller: :departments
+    resources :locations, except: [:show], controller: :locations
   end
-  devise_for :users
-
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
