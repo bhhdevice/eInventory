25bea2957183f7d98d88dc7370c7264eae49231d
goldenhub77 <sg300c@gmail.com>
2018-06-19 21:41:13 -0400

    updated view tests

--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -6,23 +6,16 @@ RSpec.describe Brand, type: :model do
 
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name).case_insensitive }
-    it { should validate_presence_of(:address) }
-    it { should validate_uniqueness_of(:address).case_insensitive }
-    it { should validate_presence_of(:city) }
-    it { should validate_presence_of(:state) }
     it { should validate_length_of(:state).is_equal_to 2 }
-    it { should validate_presence_of(:zip_code) }
     it { should_not allow_value('acbd').for(:zip_code)}
     it { should_not allow_value('1234').for(:zip_code)}
     it { should allow_value('01234').for(:zip_code)}
     it { should allow_value('01234-1234').for(:zip_code)}
-    it { should validate_presence_of(:phone_number) }
     it { should_not allow_value('acbd').for(:phone_number)}
     it { should_not allow_value('1234').for(:phone_number)}
     it { should_not allow_value('0123456789').for(:phone_number)}
     it { should allow_value('123-123-1234').for(:phone_number)}
     it { should allow_value('1-123-123-1234').for(:phone_number)}
-    it { should validate_presence_of(:website) }
     it { should_not allow_value('acbd').for(:website)}
     it { should_not allow_value('www.test').for(:website)}
     it { should_not allow_value('test.com').for(:website)}
@@ -31,6 +24,6 @@ RSpec.describe Brand, type: :model do
   end
 
   describe 'associations' do
-
+    it { should have_many(:models) }
   end
 end
