4fee888b61b6f9754ada959ce6af87c0a0137f9c
goldenhub77 <sg300c@gmail.com>
2018-07-02 10:58:17 -0400

    updated equipment tests

--- spec/models/equipment_spec.rb
+++ spec/models/equipment_spec.rb
@@ -17,4 +17,23 @@ RSpec.describe Equipment, type: :model do
     it { should belong_to(:brand) }
     it { should belong_to(:model) }
   end
+
+  describe '.model_brand_ok?' do
+    it 'returns false when model is not within selected brand' do
+      equipment = build(:equipment, brand: @brand, model: create(:model))
+      expect(equipment.send(:model_brand_ok?)).to eq false
+    end
+  end
+
+  describe '.category' do
+    it 'returns equipment model category' do
+      equipment = create(:equipment, brand: @brand, model: @model)
+      expect(equipment.category).to eq equipment.model.category
+    end
+
+    it 'returns nil when a model does not exist' do
+      equipment = build(:equipment, brand: @brand, model: nil)
+      expect(equipment.category).to eq nil
+    end
+  end
 end
--- spec/requests/equipment_spec.rb
+++ spec/requests/equipment_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe "Equipment", type: :request do
   before(:each) do
     request_login_admin
   end
-  
+
   describe "GET /equipment" do
-    it "works! (now write some real specs)" do
+    it "responded successfully." do
       get equipment_index_path
       expect(response).to have_http_status(200)
     end
